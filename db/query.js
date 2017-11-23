271684d7c800985de64eac46c3f49ef0484ec88a
Ellis Mwekunda <Scareb15@gmail.com>
2017-11-23 14:48:11 -0700

    added queries for post

--- db/query.js
+++ db/query.js
@@ -1,7 +1,19 @@
 const db = require('./connection')
 
+getAllCompanies = () => {
+    return db('users')
+}
 
+postUser = body => {
+    return db('users').insert({
+        firstname: body.firstname,
+        lastname: body.lastname,
+        company: body.company,
+        description: body.description
+    })
+}
 
 module.exports = {
-    
+    getAllCompanies,
+    postUser
 }
